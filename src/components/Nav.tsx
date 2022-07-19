2a9b8538b79f94c3ed3e8f0b91429437068ff21e
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 00:57:24 +0800

    fixed routing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
     return (
         <div className="nav-container transition-all duration-500 shadow-xl shadow-neutral-900 min-w-screen min-h-3em bg-background p-9">
             <div className="nav flex gap-[3em] justify-center items-center">
-                <a href="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">HOME</a>
-                <a href="/profile" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">PROFILE</a>
-                <a href="/blog" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">BLOG</a>
+                <Link to="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">HOME</Link>
+                <Link to="/profile" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">PROFILE</Link>
+                <Link to={"/blog"} className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">BLOG</Link>
             </div>
         </div>
     )
