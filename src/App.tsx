93804993573f2fa1f45b982503ea3d47ff5730c5
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-19 18:33:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Layout from "./components/layout/Layout"
+
+export default function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <h1>Hello, World!</h1>
+      </Layout>
+    </div>
+  )
+}
