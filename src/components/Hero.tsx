e5cb8b20349eade987e7c77fc0e10f9117082a8d
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-19 22:09:19 +0800

    added hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="top-[3em] h-[calc(100vh-6em)] flex justify-center items-center">
+            <h1>hai</h1>
+        </div>
+    )
+}
