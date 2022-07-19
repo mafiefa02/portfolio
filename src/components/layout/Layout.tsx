1ad54b5b158de236f9eaf3f234f506adaf1e02d4
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-19 18:53:39 +0800

    Added global style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import Nav from "../Nav"
 import Footer from "../Footer"
+import GlobalStyle from "../../styles/global"
 
 export default function Layout(props: any) {
     return (
         <>
             <div className="min-w-screen min-h-screen bg-background">
+                <GlobalStyle />
                 <Nav />
                 {props.children}
                 <Footer />
---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: "Montserrat";
+        color: white;
+    }
+`;
+
+export default GlobalStyle;
