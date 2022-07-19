de5a22918782ed34d46a87e7aff8098e464d7edf
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 00:51:44 +0800

    add link on navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,8 @@ export default function Nav() {
         <div className="nav-container transition-all duration-500 shadow-xl shadow-neutral-900 min-w-screen min-h-3em bg-background p-9">
             <div className="nav flex gap-[3em] justify-center items-center">
                 <a href="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">HOME</a>
-                <a href="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">PROFILE</a>
+                <a href="/profile" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">PROFILE</a>
+                <a href="/blog" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">BLOG</a>
             </div>
         </div>
     )
