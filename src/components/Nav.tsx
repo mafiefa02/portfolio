49c225031cd145d928c9f2aa54cf1c7ba063d31e
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-19 23:54:44 +0800

    changed nav shadow

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
     return (
-        <div className="nav-container shadow-sm shadow-primary min-w-screen min-h-3em bg-background p-9">
+        <div className="nav-container transition-all duration-500 shadow-xl shadow-neutral-900 min-w-screen min-h-3em bg-background p-9">
             <div className="nav flex gap-[3em] justify-center items-center">
                 <a href="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">HOME</a>
                 <a href="/" className="transition duration-500 active:duration-200 hover:opacity-100 font-bold opacity-50 active:text-primary">PROFILE</a>
