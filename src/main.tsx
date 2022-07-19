60351a3d0d55598ffcf09993308164c62911422f
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 00:51:48 +0800

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
 import './index.css'
+import ComingSoon from './pages/ComingSoon';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/blog' element={<ComingSoon />} />
+        <Route path='/profile' element={<ComingSoon />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
