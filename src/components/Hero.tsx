45427b4b622e175815d6d8188efaaf3e787eaa6e
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-19 23:55:01 +0800

    added temp hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,10 @@
 export default function Hero() {
     return (
-        <div className="top-[3em] h-[calc(100vh-6em)] flex justify-center items-center">
-            <h1>hai</h1>
+        <div className="h-[calc(100vh-6em)] flex justify-center items-center">
+            <div className="flex-col justify-center items-center font-bold">
+                <p className="text-4xl">hello, i am</p>
+                <p className="text-7xl mt-2 drop-shadow-2xl text-primary cursor-pointer hover:animate-pulse">afief abdurrahman</p>
+            </div>
         </div>
     )
 }
