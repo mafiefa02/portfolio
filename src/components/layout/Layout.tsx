b0de276749b992a0f14fe6bfa86e18292a11c791
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 10:19:01 +0800

    add sidebar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import Nav from "../Nav"
 import Footer from "../Footer"
 import GlobalStyle from "../../styles/global"
+import Sidebar from "../Sidebar"
 
 export default function Layout(props: any) {
     return (
@@ -8,6 +9,7 @@ export default function Layout(props: any) {
             <div className="min-w-screen min-h-screen bg-background">
                 <GlobalStyle />
                 <Nav />
+                <Sidebar />
                 {props.children}
                 <Footer />
             </div>
