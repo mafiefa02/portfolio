ac7576b71e13417dae22c34f906d1ff766e1f17e
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 11:38:03 +0800

    fix sidebar position

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import styles from './Sidebar.module.css'
 export default function Sidebar() {
     return (
         <>
-            <div className='transition flex flex-row fixed left-0 top-[45%] gap-2 ml-6 items-center w-auto'>
+            <div className='transition flex flex-row fixed left-0 top-[50%] gap-2 ml-6 items-center w-auto'>
                 <div className={'sidebar-icons flex flex-col gap-8 items-center justify-center rounded-full bg-neutral-900 pt-6 pb-6 pl-4 pr-4'}>
                     <a target={"_blank"} href={'https://github.com/mafiefa02'}><FaGithub className={styles.sidebarIcon} color={"white"} /></a>
                     <a target={"_blank"} href={'https://instagram.com/mafiefa'}><FaInstagram className={styles.sidebarIcon} color={"white"} /></a>
