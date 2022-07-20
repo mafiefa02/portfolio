ce1109ab72d5f8994b55172a7c3fad4f540a4c7a
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-20 11:19:46 +0800

    add responsiveness

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -4,7 +4,10 @@ export default function ComingSoon() {
     return (
         <Layout>
             <div className="h-[calc(100vh-6em)] flex justify-center items-center font-bold">
-                <p className="text-7xl mt-2 drop-shadow-2xl text-primary cursor-pointer hover:animate-pulse">coming soon.</p>
+                <div className="flex lg:flex-row md:flex-row gap-[1em] sm:flex-col flex-col">
+                    <p className="self-center sm:self-center text-5xl lg:text-7xl md:text-6xl sm:text-6xl sm:drop-shadow-lg drop-shadow-2xl text-primary cursor-pointer hover:animate-pulse">coming</p>
+                    <p className="contents text-5xl lg:text-7xl md:text-6xl sm:text-6xl sm:drop-shadow-lg drop-shadow-2xl text-primary cursor-pointer hover:animate-pulse">soon.</p>
+                </div>
             </div>
         </Layout>
     )
