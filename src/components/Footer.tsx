b5c63326360064b051c7634bb8798009d045fcf6
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-06-07 17:34:06 +0800

    migrate to nextjs

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    let newDate = new Date();
-    let year = newDate.getFullYear();
-
-    return (
-        <div className="footer-container flex p-3 text-sm justify-center items-center dark:opacity-50 dark:text-white text-black"><p>&copy; {year} Afief Abdurrahman</p></div>
-    )
-}
