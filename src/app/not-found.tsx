d8f6e423b26d1ba96932a5f4de0de330878e1827
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-06-08 23:28:20 +0800

    fix min height 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function NotFound() {
     return (
         <main>
-            <div className="flex flex-col justify-center items-center gap-4 min-h-[calc(100vh-(78px+8rem))]">
+            <div className="flex flex-col justify-center items-center gap-4 md:min-h-[calc(100vh-(78px+8rem))] min-h-[calc(100vh-(78px+4rem))]">
                 <h1 className="text-6xl">404</h1>
                 <p>Page not found</p>
                 <Button type="default" link="/" name="Back" />
