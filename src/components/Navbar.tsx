2ac2c3c33eefea56136611b3bdfaf6bd45a90d4e
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-06-08 20:04:27 +0800

    remove transition and add z-index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar(props: NavbarProps) {
 
     return (
         <>
-            <nav className={`sticky top-0 flex flex-row self-start w-full justify-between px-12 py-6 ${isNavbarOpen ? 'md:border-b-2 md:border-black md:shadow-lg md:bg-opacity-80 md:backdrop-blur-sm' : `border-b-2 border-black shadow-lg bg-opacity-80 backdrop-blur-sm`} mb-16 bg-white transition`}>
+            <nav className={`sticky top-0 flex flex-row self-start w-full justify-between px-12 py-6 ${isNavbarOpen ? 'md:border-b-2 md:border-black md:shadow-lg md:bg-opacity-80 md:backdrop-blur-sm' : `border-b-2 border-black shadow-lg bg-opacity-80 backdrop-blur-sm`} mb-16 bg-white`}>
                 <p className={`font-bold text-xl`}>Afief&apos;s <span className="font-bold hidden md:contents">| </span><span className="font-light hidden md:contents">personal place</span></p>
                 <div className="hidden md:flex flex-row gap-12">
                     {navbarButtons.map((button, index) => {
@@ -47,7 +47,7 @@ export default function Navbar(props: NavbarProps) {
                     <FontAwesomeIcon icon={isNavbarOpen ? faX : faBars} size="xl" color="#000000" onClick={toggleNavbar} />
                 </div>
             </nav>
-            <div className={`fixed top-16 w-full flex-col justify-start pt-16 items-center gap-12 h-screen md:hidden ${isNavbarOpen ? 'flex' : 'hidden'} bg-white bg-opacity-80 backdrop-blur-sm`}>
+            <div className={`fixed top-16 w-full flex-col justify-start pt-16 items-center gap-12 h-screen md:hidden ${isNavbarOpen ? 'flex' : 'hidden'} bg-white bg-opacity-80 backdrop-blur-sm z-50`}>
                 {navbarButtons.map((button, index) => {
                     return (
                         <Button type="text" key={index} link={button.link} name={button.name} className="text-xl" />
