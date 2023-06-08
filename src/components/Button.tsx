8ba8b4fd48281b4ec3ee52bbdea2a82ed93aae63
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-06-08 23:28:32 +0800

    add no-wrap on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,11 +12,11 @@ export default function Button(props: buttonProps) {
     const buttonClasses = [
         {
             type: "default",
-            className: "bg-black text-white border-2 border-black hover:bg-white hover:bg-opacity-80 hover:backdrop-blur-sm hover:text-black px-12 py-2 hover:font-bold transition"
+            className: "bg-black text-white border-2 border-black hover:bg-white hover:bg-opacity-80 hover:backdrop-blur-sm hover:text-black px-12 py-2 hover:font-bold transition whitespace-nowrap"
         },
         {
             type: "outline",
-            className: "bg-white text-black border-2 border-black hover:bg-black hover:text-white px-12 py-2 hover:font-bold transition"
+            className: "bg-white text-black border-2 border-black hover:bg-black hover:text-white px-12 py-2 hover:font-bold transition whitespace-nowrap"
         },
         {
             type: "text",
