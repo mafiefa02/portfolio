0c7d9d04d5f48da0ae5c350d234934b090c82b17
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-06-09 09:06:41 +0800

    change button hover state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button(props: buttonProps) {
     const buttonClasses = [
         {
             type: "default",
-            className: "bg-black text-white border-2 border-black hover:bg-white hover:bg-opacity-80 hover:backdrop-blur-sm hover:text-black px-12 py-2 hover:font-bold transition whitespace-nowrap"
+            className: "bg-black text-white border-2 border-black hover:backdrop-blur-sm px-12 py-2 hover:font-bold transition whitespace-nowrap"
         },
         {
             type: "outline",
